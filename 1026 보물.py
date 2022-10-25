38510bb831cb03e56f37aa4ec2c10a5632abb5cd
Minho Park <pminh98@naver.com>
2022-10-25 19:57:33 +0900

    first commit

--- 1026 보물.py
+++ 1026 보물.py
@@ -3,12 +3,10 @@ A = map(int, input().split())
 B = map(int, input().split())
 
 sum = 0
-tmp = []
 A = sorted(A)
 B = sorted(B, reverse=True)
 
 for i in range(N):
     sum += A[i]*B[i]
     
-    
-print(sum)
+print(sum)#hi
